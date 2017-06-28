2e7fc8061043e41968cf6f077328bfc2e1687ab4
xiegu <xiecheng.gu@hotmail.com>
2017-06-28 15:57:57 +0800

    back to earth

--- scrapy/tutorial/tutorial/items.py
+++ scrapy/tutorial/tutorial/items.py
@@ -8,7 +8,9 @@
 import scrapy
 
 
-class TutorialItem(scrapy.Item):
+class DmozItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
+    title = scrapy.Field()
+    link = scrapy.Field()
+    desc = scrapy.field()
     pass
